3dc60fcedb92985885e140e0ee6f8dc72ed2099a
Maxim Sylka <msylka@me.com>
2023-11-01 22:10:47 +0300

    Initial Commit

---
+++ HW2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.01
+//
+//  Created by Максим Сылка on 01.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
